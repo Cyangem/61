b6d9a65b5a44638cfd67a7fe9c0fddeea765f137
Yaxin Li <45999757+Cyangem@users.noreply.github.com>
2020-08-07 20:33:38 -0700

    hw2 is completed

---
+++ hw2/hw2/PercolationStats.java
@@ -0,0 +1,69 @@
+package hw2;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/*public class PercolationStats {
+    public PercolationStats(int N, int T, PercolationFactory pf)   // perform T independent experiments on an N-by-N grid
+    public double mean()                                           // sample mean of percolation threshold
+    public double stddev()                                         // sample standard deviation of percolation threshold
+    public double confidenceLow()                                  // low endpoint of 95% confidence interval
+    public double confidenceHigh()                                 // high endpoint of 95% confidence interval
+}*/
+
+public class PercolationStats {
+
+    private double[] fractions;
+    private int numTrials;
+
+    //perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        numTrials = T;
+        fractions = new double[T];
+        int totalSites = N * N;
+        for (int i = 0; i < T; i += 1) {
+            int numOpenedSites = 0;
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (!p.isOpen(row, col));
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                    numOpenedSites += 1;
+                }
+            }
+            //if no double cast, the division will two integers will round down to a integer
+            fractions[i] = (double) numOpenedSites / totalSites;
+        }
+    }
+
+    //sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+
+    //high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double mu = mean();
+        double sigma = stddev();
+        return mu + (1.96 * sigma / Math.sqrt(numTrials));
+    }
+
+
+    //sample endpoint of 95% confidence interval
+    public double confidenceLow() {
+        double mu = mean();
+        double sigma = stddev();
+        return mu - (1.96 * sigma / Math.sqrt(numTrials));
+    }
+
+}
